a203401a03d7aeae2737bc27bc472b5dda9056e5
ange1a1in <angela1219@berkeley.edu>
2026-07-27 17:47:07 +0800

    Add: combinations - submission-2

---
+++ Data Structures & Algorithms/combinations/submission-2.py
@@ -0,0 +1,19 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        path = []
+        result = []
+        nums = [i for i in range(1, n+1)]
+        self.dfs(nums, path, result, 0, k)
+        return result
+
+    
+    def dfs(self, nums, path, result, startIndex, k):
+        if len(path) == k:
+            result.append(path + [])
+            return
+        
+        for i in range(startIndex, len(nums)):
+            path.append(nums[i])
+            self.dfs(nums, path, result, i + 1, k)
+            path.pop(-1)
+
